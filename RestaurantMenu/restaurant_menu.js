6e07ed46cd867720a62a5481407e66c91441fcfc
Haru Sakai <harusakai0515@gmail.com>
2024-07-10 20:31:07 -0400

    practice

--- RestaurantMenu/restaurant_menu.js
+++ RestaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $32', 'Pasta - $24.99', 'Burger - $23', 'Salmon - $29.99'];
+const dessertMenu = ['Cake - $7.99', 'Ice Cream - $5', 'Pudding - $2.99', 'Fruit Salad - $3.25'];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
