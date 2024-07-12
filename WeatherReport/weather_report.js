a65ec2d96df7488ea507f95961941e1a293dc83b
Haru Sakai <harusakai0515@gmail.com>
2024-07-11 20:13:30 -0400

    practice

--- WeatherReport/weather_report.js
+++ WeatherReport/weather_report.js
@@ -20,4 +20,28 @@ function showweatherDetails(event) {
     });
 }
 
+function showweatherDetails1(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('latitude').value;
+    const lon = document.getElementById('longitude').value;
+    const apiKey = '29824f96da7c987d9251a6ce6eb0161f'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    });
+}
+
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherForm1').addEventListener('submit',showweatherDetails1 );
